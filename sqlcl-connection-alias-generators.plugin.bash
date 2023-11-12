9d26007dce420a246b798eb9cbf4281569975631
Jason Lyle <jason.lyle@oracle.com>
2023-11-12 08:54:35 -0500

    Setup plugin files for sourcing

---
+++ sqlcl-connection-alias-generators.plugin.bash
@@ -0,0 +1,46 @@
+#! /usr/bin/env bash
+
+function main() {
+    function getCanonicalPath() {
+        ########################################################################
+        #   getCanonicalPath
+        #
+        #   Return a path that is both absolute and does not contain any
+        #   symbolic links. Always returns without a trailing slash.
+        #
+        #   The first parameter is the path to canonicalize
+        #
+        #   canonicalPath="$(getCanonicalPath "${somePath}")"
+        ########################################################################
+        local target="${1}"
+
+        if [ -d "${target}" ]; then
+            # dir
+            (cd "${target}" || exit; pwd -P)
+        elif [ -f "${target}" ]; then
+            # file
+            if [[ "${target}" = /* ]]; then
+                # Absolute file path
+                (cd "$(dirname "${target}")" || exit; printf -- '%s/%s\n' "$(pwd -P)" "$(basename "${target}")")
+            elif [[ "${target}" == */* ]]; then
+                # Relative file with path
+                printf -- '%s\n' "$(cd "${target%/*}" || exit; pwd -P)/${target##*/}"
+            else
+                # Relative file without path
+                printf -- '%s\n' "$(pwd -P)/${target}"
+            fi
+        fi
+    } # getCanonicalPath
+
+    local scriptPath
+    local binDirectory
+
+    scriptPath="$(getCanonicalPath "$(dirname -- "${BASH_SOURCE[0]}")")"
+    binDirectory="${scriptPath}/bin"
+
+    if ! printf '%s' "${PATH}" | grep -Eq "(^|:)${binDirectory}($|:)"; then
+        export PATH="${binDirectory}:${PATH}"
+    fi
+} # main
+
+main "$@"
---
+++ sqlcl-connection-alias-generators.plugin.zsh
@@ -0,0 +1,23 @@
+# Standardized $0 handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+declare sqlclConnectionAliasGeneratorsPluginDirectory="${0:h}"
+
+function () {
+    # Standardize options
+    # https://wiki.zshell.dev/community/zsh_plugin_standard#standard-recommended-options
+    builtin emulate -L zsh ${=${options[xtrace]:#off}:+-o xtrace}
+    builtin setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+
+    local binDirectory
+
+    binDirectory="${sqlclConnectionAliasGeneratorsPluginDirectory}/bin"
+
+    if ! ((${path[(Ie)${binDirectory}]})); then
+        export path=("${binDirectory}" "${path[@]}")
+    fi
+}
+
+unset sqlclConnectionAliasGeneratorsPluginDirectory
